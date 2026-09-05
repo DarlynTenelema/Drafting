7853bc0b03b8fed53c82b984e31806ed282f35f5
darlyndavid2026-lab <darlyndavid2026@gmail.com>
2026-09-05 16:31:16 -0500

    primer commit

--- drafting/lib/screens/fanart_detail_screen.dart
+++ drafting/lib/screens/fanart_detail_screen.dart
@@ -6,7 +6,6 @@ import 'package:gal/gal.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:share_plus/share_plus.dart';
-import 'package:screen_protector/screen_protector.dart';
 import 'dart:convert';
 import '../theme/app_theme.dart';
 import '../core/models/fanart_model.dart';
@@ -63,11 +62,11 @@ class _FanartDetailScreenState extends State<FanartDetailScreen> {
   }
 
   Future<void> _secureScreen() async {
-    await ScreenProtector.preventScreenshotOn();
+    // await ScreenProtector.preventScreenshotOn();
   }
 
   Future<void> _unsecureScreen() async {
-    await ScreenProtector.preventScreenshotOff();
+    // await ScreenProtector.preventScreenshotOff();
   }
 
   @override
